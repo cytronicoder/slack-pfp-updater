d1442daab85a05941c6d26bb0d99fab4178f1428
Zeyu Yao <novodoodle@gmail.com>
2023-08-09 16:33:53 +0800

    Add images and update index page

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,16 @@ export default function Home() {
     <div className={styles.container}>
       <main className={styles.main}>
         <img src="/api/current/" className={styles.avatar} />
+        <div className={styles.grid}>
+          <a href={"/api/set-profile"} className={styles.card}>
+            <h3>Change the avatar &rarr;</h3>
+            <p>Changes on the Hack Club Slack. Have fun!</p>
+          </a>
+          <a href={"https://cytronicoder.com"} className={styles.card}>
+            <h3>Visit my portfolio &rarr;</h3>
+            <p>If you want to know more about who made this...</p>
+          </a>
+        </div>
       </main>
     </div>
   );

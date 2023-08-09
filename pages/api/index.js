7761b947c87236deab533f2aa6b65ed28d956da7
Zeyu Yao <novodoodle@gmail.com>
2023-08-09 15:34:43 +0800

    Add Slack integration

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const { WebClient } = require("@slack/web-api");
 const axios = require("axios").default;
 const fs = require("node:fs");
@@ -15,33 +16,26 @@ async function getRandomImage() {
 
   console.log(randomImageFile);
 
-//   const imageUrl = `https://profile-pics.cytronicoder.com/${randomImageFile}`;
-//   const image = await axios.get(imageUrl, {
-//     responseType: "arraybuffer",
-//   });
+  const imageUrl = `https://profile-pics.cytronicoder.com/${randomImageFile}`;
+  const image = await axios.get(imageUrl, {
+    responseType: "arraybuffer",
+  });
 
-//   return image.data;
+  return image.data;
 }
-
-async function main() {
-  const image = await getRandomImage();
-//   console.log(image);
-}
-
-main();
 
 /**
  * Sets the profile picture of the Slack user based on the current time.
  *
  * @return {Promise} A Promise that resolves once the profile picture is set.
  */
-// export default async (req, res) => {
-//   const image = await getRandomImage();
-//   const client = new WebClient();
-//   await client.users.setPhoto({
-//     image: image,
-//     token: process.env.SLACK_TOKEN,
-//   });
-//   res.send("Profile picture updated!");
-//   //   res.redirect("https://profile-pic.cytronicoder.com/");
-// };
+export default async (req, res) => {
+  const image = await getRandomImage();
+  const client = new WebClient();
+  await client.users.setPhoto({
+    image: image,
+    token: process.env.SLACK_TOKEN,
+  });
+  res.send("Profile picture updated!");
+  //   res.redirect("https://profile-pic.cytronicoder.com/");
+};
